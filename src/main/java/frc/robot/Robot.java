fbc09b3a2ae670ded6809e9a6451c75219b65289
cabobalot <casey8cockrell@gmail.com>
2019-01-09 09:58:05 -0700

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,8 +8,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -70,3 +69,7 @@ public class Robot extends TimedRobot {
   public void testPeriodic() {
   }
 }
+
+
+
+
